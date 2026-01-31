c1ec1cd5fdd889271e92bf90a4643c8c352208a3
senhas-rgb <senhasdiwan@gmail.com>
2026-01-31 13:43:00 +0530

    chore: restructure documentation and add new content sections for configuration, moderation commands, and usage

--- content/_meta.ts
+++ content/_meta.ts
@@ -3,9 +3,18 @@ export default {
   '##': {
     type: 'separator',
   },
-  markdown: '',
-  mantine: '',
-  api: '',
+  gettingstarted: 'Getting Started',
+  configSetup: 'Configuration and Setup',
+  modCommands: 'Moderation Commands',
+  warnsystem: 'Warning System',
+  punishsystem: 'Punishment System',
+  stat: 'Statistics And History',
+  repSystem: 'Reputation System',
+  pastpapers: 'Past Papers Search',
+  utility: 'Utility Commands',
+  owneronly: 'Owner Only Commands',
+  logging: 'Logging System',
+  usage: 'Usage Tips and Best Practices',
 
   '###': {
     type: 'separator',
